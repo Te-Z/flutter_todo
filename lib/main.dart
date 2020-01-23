615a6b6d1eec14984dc0772aa9141baa6a33a6ca
Te-Z <tezfreelance@gmail.com>
2020-01-23 18:11:27 +0100

    background fetch fonctionnel

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:background_fetch/background_fetch.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todo_poc/blocs/photo_bloc.dart';
@@ -7,10 +8,30 @@ import 'package:todo_poc/models/photo.dart';
 import 'package:todo_poc/ui/network_screen.dart';
 import 'package:todo_poc/ui/sqlite_screen.dart';
 
-void main() => runApp(MyApp());
+void backgroundFetchHeadlessTask() async {
+  print('[BackgroundFetch] Headless event received.');
+  BackgroundFetch.finish();
+}
 
-class MyApp extends StatelessWidget {
+void main() {
+  runApp(MyApp());
+  BackgroundFetch.registerHeadlessTask(backgroundFetchHeadlessTask);
+}
+
+class MyApp extends StatefulWidget {
   // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  @override
+  void initState() {
+    initPlatformState();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -33,6 +54,43 @@ class MyApp extends StatelessWidget {
       ),
     );
   }
+
+  Future<void> initPlatformState() async {
+    /// Configuration de BackgroundFetch
+    BackgroundFetch.configure(BackgroundFetchConfig(
+        minimumFetchInterval: 15, /// Sur iOS et Android l'intervale minimale est de 15 minutes
+        stopOnTerminate: false, /// Laisse la tâche active quand l'application se ferme
+        enableHeadless: true, /// Laisse la tâche active quand l'application se ferme (Android only)
+        requiresBatteryNotLow: false,
+        requiresCharging: false,
+        requiresStorageNotLow: false,
+        requiresDeviceIdle: false,
+        requiredNetworkType: BackgroundFetchConfig.NETWORK_TYPE_ANY
+    ), () async {
+
+      /// Callback contenant l'action à effectuer pendant que l'application est fermée
+      PhotoBloc _bloc = PhotoBloc();
+
+      _bloc.add(AddAllPhotoFromNetworkToDB());
+
+      /// IMPORTANT: Vous devez signaler explicitement la fin de la tâche sous peine de voir l'application pénalisée par l'OS
+      /// du fait d'une tâche trop longue en background
+      BackgroundFetch.finish();
+
+    }).then((int status) {
+
+      print('[BackgroundFetch] configure success: $status');
+    }).catchError((e) {
+
+      print('[BackgroundFetch] configure ERROR: $e');
+
+    });
+
+    /// Si le widget a été retirer de l'arborescence pendant que la tâche était en cours
+    /// il est mieux d'abandonner la réponse plutôt que d'appeler setState pour rafraichir notre
+    /// interface non existante
+    if (!mounted) return;
+  }
 }
 
 class MyHomePage extends StatefulWidget {
@@ -52,6 +110,24 @@ class _MyHomePageState extends State<MyHomePage> {
       child: Scaffold(
         appBar: AppBar(
           title: Text(widget.title),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.play_arrow),
+              onPressed: () => BackgroundFetch.start().then((int status) {
+                print('[BackgroundFetch] start success: $status');
+              }).catchError((e) {
+                print('[BackgroundFetch] start FAILURE: $e');
+              }),
+            ),
+            IconButton(
+              icon: Icon(Icons.stop),
+              onPressed: () => BackgroundFetch.stop().then((int status) {
+                print('[BackgroundFetch] start success: $status');
+              }).catchError((e) {
+                print('[BackgroundFetch] start FAILURE: $e');
+              }),
+            ),
+          ],
           bottom: TabBar(
             tabs: <Widget>[
               Tab(icon: Icon(Icons.wifi), text: "API"),
